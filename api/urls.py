90f21dc57e5d238dfc0dafd90e8da66c5e1a793d
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-31 14:25:42 +0300

    cleared commented code - api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -31,33 +31,3 @@ urlpatterns = [
     path('incomes/<int:pk>/', views.DetailIncome.as_view(),
          name='income-detail'),
 ]
-
-
-# # apis/urls.py
-# from django.urls import path
-
-# from .views import ListExpense, DetailExpense
-# from .views import ListExpenseCategory, DetailExpenseCategory
-# from .views import ListIncome, DetailIncome
-# from .views import ListIncomeCategory, DetailIncomeCategory
-
-# urlpatterns = [
-
-#     # Expenses and Expense Categories Paths
-#     path('expenses', ListExpense.as_view(), name='expense-list'),
-#     path('expense/<int:pk>/', DetailExpense.as_view(),
-# name='expense-detail'),
-#     path('expense-categories', ListExpenseCategory.as_view(),
-#          name='expensecategory-list'),
-#     path('expense-categories/<int:pk>/', DetailExpenseCategory
-#          .as_view(), name='expensecategory-detail'),
-
-
-#     # Income and Income Categories Paths
-#     path('incomes', ListIncome.as_view(), name='income-list'),
-#     path('income/<int:pk>/', DetailIncome.as_view(), name='income-detail'),
-#     path('income-categories', ListIncomeCategory.as_view(),
-#          name='incomecategory-list'),
-#     path('income-categories/<int:pk>/', DetailIncomeCategory.as_view(),
-#          name='incomecategory-detail'),
-# ]
